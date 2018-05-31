3a8cfbeceadc6416e10b2bb09bd58c2652820bfc
LukaszWeg <lukasz0596@gmail.com>
2018-05-31 15:43:48 +0200

    add worker

---
+++ Bibloteka/Data/Worker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibloteka.Data
+{
+    public class Worker
+    {
+        string firstname = "";
+        string lastname = "";
+        long telNumber = 0;
+        DateTime hired = DateTime.Now;
+
+        public string Firstname
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+
+        public string Lastname
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+
+        public long TelNumber
+        {
+            get { return telNumber; }
+            set { telNumber = value; }
+        }
+
+        public DateTime Hired
+        {
+            get { return hired; }
+            set { hired = value; }
+        }
+    }
+}
---
+++ Bibloteka/WorkerForm.cs
@@ -0,0 +1,28 @@
+﻿using Bibloteka.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bibloteka
+{
+    public partial class WorkerForm : Form
+    {
+        private Worker worker = new Worker();
+        public WorkerForm()
+        {
+            InitializeComponent();
+            this.workerBindingSource.DataSource = worker;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
